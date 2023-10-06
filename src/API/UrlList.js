ea3ebb3bd6609ae2f4f7079825c9b750dc802a09
SanketMoontechnolabs <139355299+SanketMoontechnolabs@users.noreply.github.com>
2023-10-06 17:38:50 +0530

    live code added 0102

--- src/API/UrlList.js
+++ src/API/UrlList.js
@@ -9,7 +9,7 @@
   } else {
     Object.assign(URLS, {
       BASE_URL: `${import.meta.env.VITE_REACT_APP_LIVEPOINT}/api/v1`,
-      X_API_KEY: import.meta.env.VITE_REACT_APP_API_KEY,
+      X_API_KEY: import.meta.env.VITE_REACT_APP_LIVE_API_KEY,
       SOCKETURL: import.meta.env.VITE_REACT_APP_LIVEPOINT,
     })
   }
