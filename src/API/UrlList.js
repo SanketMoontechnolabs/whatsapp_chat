9dbfa43f90a858a8b233bfcd06160dd4e6462a71
SanketMoontechnolabs <139355299+SanketMoontechnolabs@users.noreply.github.com>
2023-10-06 17:16:07 +0530

    live code added 0102

--- src/API/UrlList.js
+++ src/API/UrlList.js
@@ -1,15 +1,15 @@
-export const URLS = {}
+  export const URLS = {}
 
-if (process.env.NODE_ENV === 'development') {
-  Object.assign(URLS, {
-    BASE_URL: `${import.meta.env.VITE_REACT_APP_ENDPOINT}/api/v1`,
-    SOCKETURL: import.meta.env.VITE_REACT_APP_ENDPOINT,
-    X_API_KEY: import.meta.env.VITE_REACT_APP_API_KEY,
-  })
-} else {
-  Object.assign(URLS, {
-    BASE_URL: `${import.meta.env.VITE_REACT_APP_LIVEPOINT}/api/v1`,
-    X_API_KEY: import.meta.env.VITE_REACT_APP_API_KEY,
-    SOCKETURL: import.meta.env.VITE_REACT_APP_LIVEPOINT,
-  })
-}
+  if (process.env.NODE_ENV === 'development') {
+    Object.assign(URLS, {
+      BASE_URL: `${import.meta.env.VITE_REACT_APP_ENDPOINT}/api/v1`,
+      SOCKETURL: import.meta.env.VITE_REACT_APP_ENDPOINT,
+      X_API_KEY: import.meta.env.VITE_REACT_APP_API_KEY,
+    })
+  } else {
+    Object.assign(URLS, {
+      BASE_URL: `${import.meta.env.VITE_REACT_APP_LIVEPOINT}/api/v1`,
+      X_API_KEY: import.meta.env.VITE_REACT_APP_API_KEY,
+      SOCKETURL: import.meta.env.VITE_REACT_APP_LIVEPOINT,
+    })
+  }
